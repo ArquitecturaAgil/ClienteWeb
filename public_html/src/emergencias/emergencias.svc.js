56bf2f27796228f139fe2843224aa2d22c21e042
Jhonatan Alarcón <jhonatan.alarcon.ariza@gmail.com>
2015-10-12 21:17:50 -0500

    Se agrega tipo de documento al form

--- public_html/src/emergencias/emergencias.svc.js
+++ public_html/src/emergencias/emergencias.svc.js
@@ -2,8 +2,8 @@
     var mod = ng.module('emergenciasModule');
     
     mod.service('EmergenciasService', ['Restangular', function(Restangular){
-            this.getHistorialByUser = function(cedula, prioridad){
-                return Restangular.one("historial", cedula).get({prioridad: prioridad});                
+            this.getHistorialByUser = function(numero, prioridad, tipoDoc){
+                return Restangular.one("historial", numero).get({prioridad: prioridad, tipoDoc: tipoDoc});                
             };
     }]);
 })(window.angular);
